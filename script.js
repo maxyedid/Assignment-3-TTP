7e05abfcd1ff9c8679de8dd26965381cf391c449
Max Yedid <maxyedid1@gmail.com>
2022-01-06 15:14:34 -0500

    Added more answers to the assignment

--- script.js
+++ script.js
@@ -7,6 +7,34 @@ let contain = document.getElementById("container")
  //#3
  let secondSelect = document.querySelectorAll(".second")
 
+ //#4
  let thirdSelect = document.querySelector("ol > li.third")
 
- thirdSelect.innerText = "six"
+//#5
+//contain.innerText = "Hello"
+
+//#6
+let divSelect = document.querySelector("div.footer");
+
+divSelect.classList.add("main")
+
+//#7
+divSelect.classList.remove("main")
+
+//#8
+let list = document.createElement("li")
+
+//#9
+list.innerText = "four"
+
+//#10
+document.querySelector("ul").append(list)
+
+//#12
+let ordered = document.querySelectorAll("ol > li")
+
+/*for (let i = 0; i < ordered.length; i++) {
+    ordered[i].style.backgroundColor = "green"
+}*/
+ordered.forEach(li => li.style.backgroundColor = "green")
+//
