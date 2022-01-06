5b26a9d25441f137aac83c70b7d619c27e725294
Max Yedid <maxyedid1@gmail.com>
2022-01-06 14:26:26 -0500

    Did #3, selected all by class name second

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ let contain = document.getElementById("container")
 //#2
  let containQuery = document.querySelector("#container")
 
- 
+ //#3
+ let secondSelect = document.querySelectorAll(".second")
+secondSelect[1].innerText = "zero"
